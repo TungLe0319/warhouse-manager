9d53576d8c700595132cdd8e63f8a2d671c33620
Tung Le <tung.le0319@gmail.com>
2022-08-29 19:03:24 -0600

    warehouse-manager

---
+++ app.js
@@ -0,0 +1,58 @@
+const packages = [{
+  heavy: true,
+  priority: false,
+  fragile: false,
+  to: 'Harrington',
+  trackingNumber: '1324kjs'
+},
+{
+  heavy: false,
+  priority: true,
+  fragile: true,
+  to: 'Mark',
+  trackingNumber: '1325sdk'
+},
+{
+  heavy: true,
+  priority: false,
+  fragile: true,
+  to: 'Mick',
+  trackingNumber: 'jffd147'
+},
+{
+  heavy: false,
+  priority: false,
+  fragile: false,
+  to: 'Jake',
+  trackingNumber: 'acdc145'
+},
+{
+  heavy: true,
+  priority: true,
+  fragile: true,
+  to: 'Brittany'
+},
+{
+  heavy: false,
+  priority: true,
+  fragile: false,
+  to: 'Zach',
+  trackingNumber: '8081baz'
+},
+{
+  heavy: true,
+  priority: false,
+  fragile: true,
+  to: 'Jeremy',
+  trackingNumber: 'suz2367'
+}
+]
+draW()
+
+function draW(){
+  
+  for (let i = 0; i < packages.length; i++) {
+    const package = packages[i];
+    console.log(i);
+  }
+}
